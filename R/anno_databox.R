32e82165cbb9bcbc8dd86a0e7738c45c7dafa770
Kwanjeera W <kwanjeeraw@gmail.com>
2024-11-12 15:36:19 +0700

    update data for enrichment analysis

--- R/anno_databox.R
+++ R/anno_databox.R
@@ -3,9 +3,9 @@
 #' Annotation data for overrepresentation and enrichment analysis
 #' @format list of meta data and pair data for organisms:
 #' \describe{
-#' Gene downloaded from Ensembl Human genes (GRCh38.p13).
-#' Protein downloaded from UniProt release 2023_02.
-#' Pathway downloaded from KEGG release 106.0.
+#' Gene downloaded from Ensembl Human genes (GRCh38.p14).
+#' Protein downloaded from UniProt release 2024_05.
+#' Pathway downloaded from KEGG release 112.0.
 #' }
 #' @source \url{""}
 "anno_databox"
